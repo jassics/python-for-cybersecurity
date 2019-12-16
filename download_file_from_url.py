3d8f27e9e9b658c6e246ccfa3443a3a9a8129a63
jassi <jassics@gmail.com>
2019-12-16 20:33:15 +0530

    minor updates alongwith requirements.txt

--- download_file_from_url.py
+++ download_file_from_url.py
@@ -1,7 +1,7 @@
 import sys
 import requests
 import re
-from colorama import init, Fore, Back, Style 
+from colorama import init, Fore, Back, Style
 import validators
 
 # PURPOSE of this script
@@ -16,25 +16,25 @@ if len(sys.argv) == 2:
   file_url = sys.argv[1] # Getting the url from command line# extract file name form the url using re module
 
   # Proceed if url is valid
-  if not validators.url(file_url): 
+  if not validators.url(file_url):
     exit(Fore.RED + Style.BRIGHT + "Not a valid url")
 
   # this regex would also work as it with positive lookbehind (?:[^/][\d\w\.-]+)$(?<=\.\w{3,4})
   # file name with extension
   matched_file = re.search("(?=[\w\d-]+\.\w{3,4}$).+", file_url) # regex with positive lookahead
   if matched_file is not None:
-    file_name_with_extension = matched_file.group(0) 
+    file_name_with_extension = matched_file.group(0)
   else:
       exit(Fore.CYAN + Style.BRIGHT + "Nothing to download")
   print("Downloading %s" % file_name_with_extension)
-  
+
   response = requests.get(file_url, stream = True)
 
   with open(file_name_with_extension, "wb") as file_download:
     total_length = response.headers.get('content-length')
     total_length = int(total_length)
     print("Total length of the file is: {0:.3f} KB".format(total_length/1024))
-   
+
     dl_bar = 0 # download bar
 
     for chunk in response.iter_content(chunk_size = 1024): #writing one chunk at a time to file_download
--- get_all_links.py
+++ get_all_links.py
@@ -2,19 +2,35 @@ import requests
 import sys
 import re
 from bs4 import BeautifulSoup as bs
+import validators
 
+# Make sure url is passed as an argument
 if len(sys.argv) != 2 :
     print("Please pass url")
     print("Ex: python {} <url>".format(sys.argv[0]))
     exit(0)
 
+# save the command line argument for url in a variable
 url = sys.argv[1]
 
-getpage = requests.get(url)  
-getpage_soup = bs(getpage.text, 'html.parser')
+# Exit if url is not valid
+if not validators.url(url):
+    exit("Not a valid url")
+
+# get response of the url
+get_content = requests.get(url)
+
+# get html formatted content using bs module
+getpage_soup = bs(get_content.text, 'html.parser')
+
+# fina all the links using a tag and with regex to look for href value as starting with http or https
 all_links= getpage_soup.findAll('a', attrs={'href' : re.compile("^https?://")})
 
+# Save the links in a file
+domain_name = url.split('//')[1]
+file_name = domain_name + '-links.txt'
 
-for link in all_links:
-    print(link.get('href'))
-
+with open(file_name, 'w+') as file_to_write:
+    for link in all_links:
+        href_link = link.get('href')
+        file_to_write.write(href_link + "\n")
