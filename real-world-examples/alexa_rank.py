56c5b9c5a5049550264510314668458588041ecf
jassi <jassics@gmail.com>
2020-08-05 21:52:53 +0530

    Changed to show global rank for all

--- real-world-examples/alexa_rank.py
+++ real-world-examples/alexa_rank.py
@@ -26,21 +26,26 @@ if not is_valid_domain(site_name):
 
 url_for_rank = alexa_base_url + site_name
 
+# Request formatted url for rank(s)
 page = requests.get(url_for_rank)
 soup = BeautifulSoup(page.content, 'html.parser')
-country_rank = soup.find_all('span', class_='pull-right')
-global_rank = soup.find_all('div', class_='rankmini-global')
 
+# get ranks text in a list
+country_ranks = soup.find_all('div', id='CountryRank')
+
+# select the data with class='rank-global' and the class='data'
+global_rank = soup.select('.rank-global .data')
+
+# Display Global rank safely
 try:
     match = re.search(r'[\d,]+', global_rank[0].text.strip())
     print("Global Rank: ", match.group())
 except:
     print("No global rank found for ", site_name)
 
+# Display country rank(s)
 try:
-    ranks = soup.find_all('div', id='CountryRank')
-    ranks_list = ranks[0].text.strip().split("\n")
-
+    ranks_list = country_ranks[0].text.strip().split("\n")
     print("Country Rank: ")
     for rank in ranks_list:
         if re.search(r'#\d+', rank):
