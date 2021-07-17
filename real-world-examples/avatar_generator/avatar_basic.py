ed8b6e26d28d8bc6de873fd416a8289dcb59555d
jassi <jassics@gmail.com>
2021-07-17 23:00:07 +0530

    Updated avatar scripts

--- real-world-examples/avatar_generator/avatar_basic.py
+++ real-world-examples/avatar_generator/avatar_basic.py
@@ -2,4 +2,5 @@ from py_avataaars import PyAvataaar
 
 avatar = PyAvataaar()
 avatar.render_png_file('basic_avatar.png')
+avatar.render_svg_file('basic_avatar.svg')
 
--- real-world-examples/avatar_generator/avatar_custom.py
+++ real-world-examples/avatar_generator/avatar_custom.py
@@ -1,9 +1,9 @@
 import py_avataaars as pa
 
 avatar = pa.PyAvataaar(
-    style=pa.AvatarStyle.CIRCLE,
+    style=pa.AvatarStyle.TRANSPARENT,
     skin_color=pa.SkinColor.LIGHT,
-    hair_color=pa.HairColor.BROWN,
+    hair_color=pa.HairColor.BLACK,
     facial_hair_type=pa.FacialHairType.DEFAULT,
     facial_hair_color=pa.HairColor.BLACK,
     top_type=pa.TopType.SHORT_HAIR_SHORT_FLAT,
@@ -12,9 +12,9 @@ avatar = pa.PyAvataaar(
     eye_type=pa.EyesType.DEFAULT,
     eyebrow_type=pa.EyebrowType.DEFAULT,
     nose_type=pa.NoseType.DEFAULT,
-    accessories_type=pa.AccessoriesType.DEFAULT,
-    clothe_type=pa.ClotheType.GRAPHIC_SHIRT,
-    clothe_color=pa.Color.HEATHER,
+    accessories_type=pa.AccessoriesType.SUNGLASSES,
+    clothe_type=pa.ClotheType.SHIRT_V_NECK,
+    clothe_color=pa.Color.BLUE_03,
     clothe_graphic_type=pa.ClotheGraphicType.BAT,
 )
 avatar.render_png_file('avatar_custom.png')
